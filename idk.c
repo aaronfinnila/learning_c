681940104163813e6dbc328584e557ca140c1e5a
Aaron <aaronkoira@gmail.com>
2025-06-23 13:49:02 +0300

    idk

---
+++ idk.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    char first[1000] = {0};
+    char second[1000] = {0};
+    int c;
+    int num = 0;
+
+    while ((c = getchar()) != EOF) {
+        if (c != '\t' && c != ' ' && c != '\n') {
+            first[num] = c;
+            num++;
+        }
+    }
+
+    for (int i=0;i<1000;i++) {
+        if (first[i] != 0) {
+            putchar(first[i]);
+        }
+    }
+
+    return 0;
+}
