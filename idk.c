db6c7cb1ee2c899a90767c06c560bf2ec8970f31
Aaron <aafinn@utu.fi>
2025-07-18 10:51:13 +0300

    testing idk file

--- idk.c
+++ idk.c
@@ -5,6 +5,7 @@ int main() {
     char second[1000] = {0};
     int c;
     int num = 0;
+    printf("\n%d", num);
 
     while ((c = getchar()) != EOF) {
         if (c != '\t' && c != ' ' && c != '\n') {
@@ -13,11 +14,13 @@ int main() {
         }
     }
 
+
     for (int i=0;i<1000;i++) {
         if (first[i] != 0) {
             putchar(first[i]);
         }
     }
+
 
     return 0;
 }
