8ed566d7add08e300612282a00f9adea124b2556
aaronfinnila <aaronfinnilaa@gmail.com>
2025-08-31 20:45:10 +0300

    ex 21 progress

--- ex1-21.c
+++ ex1-21.c
@@ -1,26 +1,30 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 #define MAX 10000
 
 int main() {
     int i = 0;
     int c = 0;
-    int rem = 0;
-    int n = 8;
+    int n = 7;
+    bool blankcount = false;
     char array[MAX];
+
     for (i; i < MAX && (c = getchar()) != EOF; ++i) {
         if (i == n) {
             n += 8;
+            blankcount = false;
         }
-        rem = n - i;
         if (c == ' ') {
-                array[i] = '\t';
-                i++;
-            if (i == n) {
-                n += 8;
+            if (blankcount == false) {
+            array[i] = '\t';
+            blankcount = true;
+            } else {
+                array[i] = c;
             }
         } else {
             array[i] = c;
+            blankcount = false;
         }
     }
 
@@ -29,11 +33,13 @@ int main() {
         if (z == ' ') {
             printf("\nblank");
         } else if (z == '\t') {
-        printf(" tab");
+        printf("\ntab");
         } else {
             printf("\n%c", z);
         }
+        printf("%d", x);
     }
+
     for (int x = 0; x <= i; x++) {
         int z = array[x];
         printf("%c", z);
